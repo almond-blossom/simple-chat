85270de0026df481fae9d1fa96bb8ae52e8faddc
edunga1 <goonr21@gmail.com>
2019-03-24 18:32:45 +0900

    Docker stack deploy

--- README.md
+++ README.md
@@ -2,15 +2,14 @@
 간단한 채팅 앱으로 스케일 아웃에 따른 세션 이슈를 테스트 해보기 위한 프로젝트입니다.
 
 0. 1개의 인스턴스, (로드밸런서 없이) 정상 동작하는 버전
-0. PM2의 클러스터링, 정상 동작하는 버전 (NodeJS의 Clustering)
 0. Docker swarm으로 3개의 인스턴스 스케일 아웃, 세션 이슈가 발생하는 버전
-0. Docker swarm의 sticky session을 이용, 정상 동작하는 버전
 0. Docker swarm과 Redis를 이용한 스케일 아웃, 정상 동작하는 버전
 
 ## START APP
 
 ### 각 버전 실행 방법
 
-`npm run app1` 1번 앱
+`npm run app1` 1번 앱<br>
+`npm run app2` 2번 앱, 끝나면 삭제 필요 `docker stack rm server`
 
 실행 후 `http://localhost:3131`에서 앱 확인.
--- server/index.js
+++ server/index.js
@@ -9,15 +9,26 @@ const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
+let online = 0;
+
 app.use(express.static(path.join(__dirname, '..', 'client')));
 
 io.on('connection', (socket) => {
+  online++;
+  console.log(online);
+
   socket.on('send', (uid, message) => {
     const pokemonIndex = (uid % cntPokemons)
     const username = pokemons[pokemonIndex];
     io.emit('chat', `${username}: ${message}`);
   });
+
+  socket.on('disconnect', () => {
+    online--;
+    console.log(online);
+  });
 });
+
 
 server.listen(3131, () => {
   console.log('server is running:', 'http://localhost:3131');
